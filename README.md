81c5a49b719071241ff6433f321dd03e9b7a8788
Eliza Struthers-Jobin <eliza.struthersjobin@gmail.com>
2017-01-05 20:01:38 -0500

    v1 react tictactoe

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple Tic Tac Toe using React
+
+- v.1 simple stateful interface (✔️)
+- v.1.2 alert for winner (WIP)
+- v.1.3 reset initial state (WIP)
+
+***
+
+basic setup using [create-react-app](https://facebook.github.io/react/docs/installation.html)
+
+```
+npm install -g create-react-app
+create-react-app name_of_app
+cd name_of_app
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            PLAYER_ONE_SYMBOL: "X",
+            PLAYER_TWO_SYMBOL: "O",
+            currentTurn: "X",
+            board: [
+                "","","",
+                "","","",
+                "","",""
+            ]
+        }
+    }
+
+    handleClick(index) {
+        if (this.state.board[index] === "") {
+            this.state.board[index] = this.state.currentTurn
+            this.setState({
+                board: this.state.board,
+                currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL
+                    ? this.state.PLAYER_TWO_SYMBOL
+                    : this.state.PLAYER_ONE_SYMBOL
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="title">
+                    <h1>Tic Tac Toe</h1>
+                </div>
+                <div className="board">
+                    {this.state.board.map((cell, index) => {
+                        return <div onClick={() => this.handleClick(index)} className="square">{cell}</div>
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
