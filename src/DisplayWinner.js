5c4c17d8045e750d7e515a4532f6fd704a571b53
Eliza Struthers-Jobin <eliza.struthersjobin@gmail.com>
2017-01-12 18:06:33 -0500

    clean up debugging

--- src/DisplayWinner.js
+++ src/DisplayWinner.js
@@ -5,7 +5,6 @@ export default class DisplayWinner extends Component {
   render() {
     return (
       <div className={this.props.winner ? 'visible' : 'hidden'}>
-        {console.log(this.props.winner)}
         <h2>Winner : { this.props.winner }</h2>
       </div>
     )
